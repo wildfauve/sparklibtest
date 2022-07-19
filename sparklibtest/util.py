b950687f46dcb6220449426d1eefb3f8fef0c154
Col Perks <wild.fauve@gmail.com>
2022-07-19 19:03:40 +1200

    first commit

---
+++ sparklibtest/util.py
@@ -0,0 +1,6 @@
+import pyspark
+from pyspark.sql.functions import lit
+import pendulum
+
+def with_lineage(df):
+    return df.withColumn("_loadtime", lit(pendulum.now("Europe/Paris")))
---
+++ tests/test_sparklibtest.py
@@ -0,0 +1,5 @@
+from sparklibtest import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
